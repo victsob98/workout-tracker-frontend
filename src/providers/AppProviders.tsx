8039ac2e32fd7875a8716243bf89f2cc77dd2160
Victoria <victoria.budziak@revolve.pro>
2024-04-08 16:53:37 +0200

    feat: add app provider

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -2,9 +2,10 @@ import { Provider as PaperThemeProvider } from "react-native-paper";
 import { AppProvidersProps } from "./AppProviders.types";
 import { LocaleContextController } from "@context/locale/localeContextController/LocaleContextController";
 import { ApiClientContextController } from "@context/apiClient/apiClientContextController/ApiClientContextController";
+import { theme } from "@theme/Theme";
 
 export const AppProviders = ({ children }: AppProvidersProps) => (
-  <PaperThemeProvider>
+  <PaperThemeProvider theme={theme}>
     <LocaleContextController>
       <ApiClientContextController>{children}</ApiClientContextController>
     </LocaleContextController>
---
+++ src/theme/Theme.ts
@@ -0,0 +1,15 @@
+import { MD3DarkTheme as DefaultTheme } from "react-native-paper";
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#C51B46",
+    onPrimary: "#A21639",
+    secondary: "#f8f4e3",
+    inactiveText: "#8f8c7a",
+    tertiary: "#E43A64",
+    background: "#282a28",
+    error: "#fc7474",
+  },
+};
